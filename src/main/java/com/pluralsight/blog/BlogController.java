f63904ac47c8bee560b13744256997135ab4da8e
Perez, Arianna <arianna.perez@accenture.com>
2021-05-18 13:20:31 -0400

    Completed module

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,13 +1,21 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class BlogController {
+    private PostRepository postRepository;
+
+    public void postRepository(PostRepository){
+        return;
+    }
+
     @RequestMapping("/")
     public String listPosts(ModelMap modelMap) {
+        getAllPosts();
         modelMap.put("title", "Blog Post 1");
         return "home";
     }
