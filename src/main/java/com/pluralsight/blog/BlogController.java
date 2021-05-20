4b3493706ecac806a181a5be3f8dfda10ba5c671
Perez, Arianna <arianna.perez@accenture.com>
2021-05-20 13:05:57 -0400

    Completed module

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -25,6 +25,8 @@ public class BlogController {
 
     @RequestMapping("/post/{id}")
     public String postDetails(@PathVariable Long id, ModelMap modelMap){
+        Post post = postRepository.findById(id);
+        modelMap.put("post", post);
         return "post-details";
     }
 
